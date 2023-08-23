c8277e648e9f3863d2d276e61566b682d6b44e18
Eze Capretta <caprettaeze@gmail.com>
2023-08-23 15:33:28 -0300

    levantando el servidor

---
+++ server/src/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const URI="mongodb://127.0.0.1:27017"
+
+  const connectToMongoDB = async () => {
+    try {
+      await mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Conectado a MongoDB');
+    
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error.message);
+  }
+};
+
+module.exports = connectToMongoDB;
